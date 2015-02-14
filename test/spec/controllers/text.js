2d1d950bab47a6bc91e88fe0cac5273d13ad0ef1
gernsdorfer <larswiedemann@gmx.net>
2015-02-14 13:25:47 +0100

    remove phone-book

--- test/spec/controllers/text.js
+++ test/spec/controllers/text.js
@@ -4,19 +4,16 @@ describe('Controller: phoneBook', function () {
 
     beforeEach(module('unchatbar-contact'));
 
-    var textCTRL, scope, PhoneBookService,MessageService;
+    var textCTRL, scope, MessageService;
 
-    beforeEach(inject(function ($controller, $rootScope, Message, PhoneBook) {
-        PhoneBookService = PhoneBook;
+    beforeEach(inject(function ($controller, $rootScope, Message) {
         MessageService = Message;
         scope = $rootScope.$new();
 
         textCTRL = function () {
             $controller('unDataChatText', {
                 $scope: scope,
-                $stateParams: stateParams,
-                $state : state,
-                PhoneBook: PhoneBookService
+                Message: MessageService
 
             });
         };
