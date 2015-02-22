519409c54ade5d4f8a8791e8352bf481542a6a77
gernsdorfer <larswiedemann@gmx.net>
2015-02-22 20:04:09 +0100

    add templates

--- app/scripts/template.js
+++ app/scripts/template.js
@@ -3,7 +3,23 @@ angular.module('unchatbar-data-chat').run(['$templateCache', function($templateC
 
   $templateCache.put('views/unchatbar-data-chat/message-box.html',
     "<div>\n" +
-    "    work\n" +
+    "    <div data-ng-repeat=\"message in messageList\" style=\"border:1px solid red\">\n" +
+    "        From:{{userMap[message.from].label}} <br>\n" +
+    "        Message:{{message.message.text}}\n" +
+    "    </div>\n" +
+    "    <textarea data-ng-model=\"text\"></textarea>\n" +
+    "    <button type=\"submit\" class=\"btn btn-default\" ng-click=\"sendTextMessage(userMap,channel);text='';\">send</button>\n" +
+    "</div>"
+  );
+
+
+  $templateCache.put('views/unchatbar-data-chat/unread-message.html',
+    "<div>\n" +
+    "    <b>unread Message ({{unreadMessageList.length || 0}})</b>\n" +
+    "    <div data-ng-repeat=\"message in unreadMessageList\">\n" +
+    "        From:{{userMap[message.from].label}} <br>\n" +
+    "        Message:{{message.message.text}}\n" +
+    "    </div>\n" +
     "</div>"
   );
 
