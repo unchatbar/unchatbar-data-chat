d9dcb4279119d3a1306a33b840470ce9211766af
gernsdorfer <larswiedemann@gmx.net>
2015-03-30 23:34:16 +0200

    add style sass for unread message

--- app/scripts/template.js
+++ app/scripts/template.js
@@ -2,25 +2,22 @@ angular.module('unchatbar-data-chat').run(['$templateCache', function($templateC
   'use strict';
 
   $templateCache.put('views/unchatbar-data-chat/message-box.html',
-    "<div class=\"text-center\" data-ng-click=\"limit=limit+10\" title=\"load more from history\">\n" +
-    "    <i class=\"fa fa-history fa-2x\"></i>\n" +
+    "<div class=\"limit-loader\" data-ng-click=\"limit=limit+10\" title=\"{{'load more from history' | translate}}\">\n" +
+    "    <i></i>\n" +
     "</div>\n" +
     "\n" +
-    "<ul class=\"list-unstyled\" data-ng-init=\"limit=10\">\n" +
-    "\n" +
+    "<ul class=\"data-chat-list\" data-ng-init=\"limit=10\">\n" +
     "    <li class=\"mar-btm\" data-ng-repeat=\"message in messageList | limitTo:(limit*-1) | orderBy:'sendStamp'\">\n" +
     "        <div data-ng-class=\"{'media-left' : ownPeerId === message.from,'media-right' : ownPeerId !== message.from}\" >\n" +
     "            <img class=\"img-circle img-sm\" ng-src=\"{{userMap[message.from].image}}\" width=\"80\">\n" +
     "        </div>\n" +
-    "        <div class=\"media-body pad-hor\" data-ng-class=\"{'speech-right' : ownPeerId !== message.from}\">\n" +
+    "        <div class=\"data-message-body pad-hor\" data-ng-class=\"{'speech-right' : ownPeerId !== message.from}\">\n" +
     "            <div class=\"speech\">\n" +
     "                <a href=\"#\" class=\"media-heading\">{{userMap[message.from].label}}</a>\n" +
-    "\n" +
     "                <p>\n" +
     "                    <ng-emoticons emoticons-template-url=\"views/unchatbar-data-chat/message.html\"\n" +
     "                                  emoticons-data=\"message.message.text\" emoticons-options=\"options\"/>\n" +
     "                </p>\n" +
-    "\n" +
     "                <p class=\"speech-time\">\n" +
     "                    <i class=\"fa fa-clock-o fa-fw\"></i> {{getFormateDate(message.message.meta.date) |\n" +
     "                    date:'dd/MM/yyyy @ H:mm' :'GMT'}}\n" +
@@ -110,67 +107,52 @@ angular.module('unchatbar-data-chat').run(['$templateCache', function($templateC
 
 
   $templateCache.put('views/unchatbar-data-chat/send-box.html',
-    "<form class=\"send-box\">\n" +
-    "    <div class=\"form-group\">\n" +
-    "        <label class=\"sr-only\" for=\"input-message\" translate>Your message</label>\n" +
-    "\n" +
-    "        <div class=\"input-group\">\n" +
+    "<div class=\"send-box\">\n" +
+    "    <form>\n" +
+    "        <label for=\"input-message\" translate>Your message</label>\n" +
+    "        <div class=\"send-input\">\n" +
     "                <textarea enter-submit=\"sendTextMessage(userMap,channel);text='';\"\n" +
     "                          id=\"input-message\"\n" +
-    "                          class=\"form-control input-sm\"\n" +
     "                          cols=\"1\"\n" +
+    "                          c\n" +
     "                          msd-elastic=\"\\n\"\n" +
     "                          autocomplete=\"off\"\n" +
     "                          data-ng-model=\"text\" class=\"form-control input-sm\"\n" +
     "                          placeholder=\"{{'Enter your message' | translate }}\"></textarea>\n" +
-    "          <span class=\"input-group-btn\">\n" +
-    "            <button type=\"button\" class=\"btn btn-inverse btn-sm\" data-ng-click=\"sendTextMessage(userMap,channel);text='';\">\n" +
+    "          <span class=\"send-button\">\n" +
+    "            <button type=\"button\"data-ng-click=\"sendTextMessage(userMap,channel);text='';\">\n" +
     "                <i class=\"fa fa-comment\"></i></button>\n" +
     "          </span>\n" +
     "        </div>\n" +
-    "    </div>\n" +
-    "</form>\n"
+    "    </form>\n" +
+    "</div>\n"
   );
 
 
   $templateCache.put('views/unchatbar-data-chat/unread-message.html',
-    "<div data-ng-init=\"showMessage=false\">\n" +
-    "    <div class=\"notification-icon fa-2x\" data-ng-click=\"showMessage=!showMessage\">\n" +
-    "        <span class=\"glyphicon glyphicon-envelope\"></span>\n" +
-    "        <span class=\"badge\">{{unreadMessageList.length || 0}}</span>\n" +
-    "    </div>\n" +
-    "    <div class=\"panel panel-primary new-message-box\" data-ng-show=\"showMessage\">\n" +
-    "        <div class=\"panel-body\" scroll-glue>\n" +
-    "            <ul class=\"chat\">\n" +
-    "                <li class=\"left clearfix\" data-ng-show=\"unreadMessageList.length === 0\">\n" +
-    "                    no new Messages\n" +
-    "                </li>\n" +
-    "                <li class=\"left clearfix\" data-ng-repeat=\"message in unreadMessageList | orderBy:'sendStamp'\">\n" +
-    "                    <a ui-sref=\"channel({channel: message.channel})\">\n" +
-    "                            <span class=\"chat-img pull-left\">\n" +
-    "                            <img class=\"profile-image\" ng-src=\"{{userMap[message.from].image}}\" width=\"80\">\n" +
-    "                        </span>\n" +
+    "<ul role=\"menu\" data-ng-init=\"showMessage=false\"\n" +
+    "    class=\"data-chat-list-unread\"\n" +
+    "        style=\"display: block;\">\n" +
+    "    <li class=\"chat-header\">\n" +
+    "        <span class=\"chat-text\" translate>You have {{unreadMessageList.length || 0}} Messages</span>\n" +
+    "    </li>\n" +
+    "    <li class=\"chat-divider\"></li>\n" +
+    "    <li data-ng-repeat=\"message in unreadMessageList | orderBy:'sendStamp'\">\n" +
+    "        <a ui-sref=\"channel({channel: message.channel})\" class=\"text-item\">\n" +
+    "              <span class=\"profile-image\">\n" +
+    "                <img alt=\"{{userMap[message.from].label}}\"\n" +
+    "                     ng-src=\"{{userMap[message.from].image}}\" class=\"img-circle img-sm\">\n" +
+    "              </span>\n" +
     "\n" +
-    "                        <div class=\"chat-body clearfix\">\n" +
-    "\n" +
-    "                            <div class=\"header\">\n" +
-    "                                <strong class=\"primary-font\">{{userMap[message.from].label}}</strong>\n" +
-    "                                <small class=\"pull-right text-muted\">\n" +
-    "                                    <span class=\"glyphicon glyphicon-time\"></span>\n" +
-    "                                    {{getFormateDate(message.message.meta.date) | date:'dd/MM/yyyy @ H:mm' :'GMT'}}\n" +
-    "                                </small>\n" +
-    "\n" +
-    "                            </div>\n" +
-    "                            <p>\n" +
-    "                                {{message.message.text}}\n" +
-    "                            </p>\n" +
-    "                        </div>\n" +
-    "                    </a>\n" +
-    "                </li>\n" +
-    "            </ul>\n" +
-    "        </div>\n" +
-    "    </div>\n" +
-    "</div>"
+    "            <div class=\"text-body\">\n" +
+    "                <div class=\"text-sm text-nowrap\">{{message.message.text}}</div>\n" +
+    "                <small class=\"text-muted\">\n" +
+    "                    {{getFormateDate(message.message.meta.date) | date:'dd/MM/yyyy @ H:mm' :'GMT'}}\n" +
+    "                </small>\n" +
+    "            </div>\n" +
+    "        </a>\n" +
+    "    </li>\n" +
+    "</ul>\n"
   );
 
 }]);
